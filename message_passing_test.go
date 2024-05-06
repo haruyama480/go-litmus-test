fdf73de35648bca233d5739fa6a125b384bca5f8
haruyama480 <haruyama480@gmail.com>
2024-05-06 21:49:46 +0900

    fix

--- message_passing_test.go
+++ message_passing_test.go
@@ -6,28 +6,29 @@ import (
 	"testing"
 )
 
-var x, y int32
-
 // On ARM, this test fails because the memory model is relaxed, which means that the CPU can reorder memory accesses.
 // https://research.swtch.com/hwmm#relaxed
 func TestMessagePassing(t *testing.T) {
-	for i := 0; i < 10000; i++ {
-		atomic.StoreInt32(&x, 0)
-		atomic.StoreInt32(&y, 0)
+	for i := 0; i < 1000000; i++ {
+		x := int64(0)
+		y := int64(0)
 
 		wg := sync.WaitGroup{}
 		wg.Add(1)
 		go func() {
-			atomic.StoreInt32(&x, 1)
-			atomic.StoreInt32(&y, 1)
+			atomic.StoreInt64(&x, 1)
+			atomic.StoreInt64(&y, 1)
 			wg.Done()
 		}()
 
-		for j := 0; j < 1000; j++ {
-			r1 := atomic.LoadInt32(&x)
-			r2 := atomic.LoadInt32(&y)
-			if r1 == 0 && r2 == 1 {
+		for j := 0; ; j++ {
+			r1 := atomic.LoadInt64(&y)
+			r2 := atomic.LoadInt64(&x)
+			if r1 == 1 && r2 == 0 {
 				t.Fatalf("ordering is broken. r1=%d, r2=%d", r1, r2)
+			}
+			if r1 == 1 {
+				break
 			}
 		}
 
