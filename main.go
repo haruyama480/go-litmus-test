37ff8c4d09913aa9239837b3f2906b827fe7f154
haruyama480 <haruyama480@gmail.com>
2024-05-06 23:37:35 +0900

    update

--- main.go
+++ main.go
@@ -6,11 +6,14 @@ import (
 	"sync/atomic"
 )
 
-const LEN = 4096
+var x [LEN]int64
+
+const LEN = 1024 * 32
+const A = 0
+const B = 1024 * 31
+
 const EPOCH = 100000
 const EPOCH_N = 60
-
-var x [LEN]int64
 
 func main() {
 	for i := int64(0); i < EPOCH*EPOCH_N; i++ {
@@ -18,21 +21,21 @@ func main() {
 			log.Printf("i=%d", i)
 		}
 
-		atomic.StoreInt64(&x[0], 0)
-		atomic.StoreInt64(&x[2048], 0)
+		atomic.StoreInt64(&x[A], 0)
+		atomic.StoreInt64(&x[B], 0)
 
 		wg := sync.WaitGroup{}
 		wg.Add(1)
 		go func() {
-			atomic.StoreInt64(&x[0], 1)
-			atomic.StoreInt64(&x[2048], 1)
-			atomic.StoreInt64(&x[0], 2)
+			atomic.StoreInt64(&x[A], 1)
+			atomic.StoreInt64(&x[B], 1)
+			atomic.StoreInt64(&x[A], 2)
 			wg.Done()
 		}()
 
 		for j := 0; ; j++ {
-			r1 := atomic.LoadInt64(&x[0])
-			r2 := atomic.LoadInt64(&x[2048])
+			r1 := atomic.LoadInt64(&x[A])
+			r2 := atomic.LoadInt64(&x[B])
 			if r1 == 2 && r2 == 0 {
 				log.Fatalf("ordering is broken. r1=%d, r2=%d, (i,j)=(%d,%d)", r1, r2, i, j)
 			}
